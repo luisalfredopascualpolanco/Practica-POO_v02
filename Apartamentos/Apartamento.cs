8324a3e683214981b3aae0b160540756e6991864
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2020-03-03 01:22:30 -0400

    primer commit

---
+++ Apartamentos/Apartamento.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apartamentos
+{
+    class Apartamento
+    {
+        private string _Direccion;
+        private string _Color;
+        private float _AreaConstruccion;
+        private byte _Parqueos;
+        private byte _Habitaciones;
+        private byte _Baños;
+
+       
+        public Apartamento(string direccion, string color, float areaconstruccion, byte parqueos, byte habitaciones, byte baños) 
+        {
+            _Direccion = direccion;
+            _Color = color;
+            _AreaConstruccion = areaconstruccion;
+            _Parqueos = parqueos;
+            _Habitaciones = habitaciones;
+            _Baños = baños; 
+        }
+        
+
+        public void Vender() 
+        {
+            Console.WriteLine();
+            Console.WriteLine("Se vende apartamento con las siguientes caracteristicas.");
+            Console.WriteLine("Direccion: "+_Direccion);
+            Console.WriteLine("Color: " + _Color);
+            Console.WriteLine("Tiene " + _AreaConstruccion+" metros cuadrados");
+            Console.WriteLine("Parqueos: " + _Parqueos);
+            Console.WriteLine("Habitaciones: " + _Habitaciones);
+            Console.WriteLine("Baños: " + _Baños);
+        }
+
+        public void Alquilar() 
+        {
+            Console.WriteLine();
+            Console.WriteLine("Se alquila apartamento con las siguientes caracteristicas.");
+            Console.WriteLine("Direccion: " + _Direccion);
+            Console.WriteLine("Color: " + _Color);
+            Console.WriteLine("Tiene " + _AreaConstruccion + " metros cuadrados");
+            Console.WriteLine("Parqueos: " + _Parqueos);
+            Console.WriteLine("Habitaciones: " + _Habitaciones);
+            Console.WriteLine("Baños: " + _Baños);
+        }
+    }
+}
---
+++ Apartamentos/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apartamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string direccion;
+            string color;
+            float areadeconstruccion;
+            byte parqueos;
+            byte habitaciones;
+            byte baños;
+            int desicion;
+
+            Console.Write("¿Donde está ubicado el apartamento? ");
+            direccion = Console.ReadLine();
+            Console.Write("¿De color es el apartamento? ");
+            color = Console.ReadLine();
+            Console.Write("¿Cual es el tamaño de construccion del apartamento? ");
+            areadeconstruccion = float.Parse(Console.ReadLine());
+            Console.Write("¿Cuantos parqueos tiene el apartamento? ");
+            parqueos = byte.Parse(Console.ReadLine());
+            Console.Write("¿Cuántas habitaciones tiene el apartamento? ");
+            habitaciones = byte.Parse(Console.ReadLine());
+            Console.Write("¿Cuántos baños tiene el apartamento? ");
+            baños = byte.Parse(Console.ReadLine());
+
+            Console.Write("Presione ´1´ si va vender o ´2´ si va a alquilar: ");
+            desicion = int.Parse(Console.ReadLine());           
+
+            Apartamento apartamento=new Apartamento(direccion, color, areadeconstruccion,parqueos,habitaciones,baños);
+
+            switch (desicion)
+            {
+                case(1):                   
+                    apartamento.Vender();
+                    break;
+
+                case(2):                                   
+                    apartamento.Alquilar();
+                    break;
+
+                default:
+                    Console.WriteLine("Usted no ha seleccionado las opciones que tiene disponible.");
+                    break;
+
+            } Console.ReadKey();
+                                   
+        }
+    }
+}
